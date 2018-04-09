143f87418d012a32e1829e42736cfe9ed4a3b627
DaveJ <dave@davejeffery.com>
2018-04-09 18:16:08 +0100

    Update deps

--- cli.js
+++ cli.js
@@ -3,26 +3,33 @@
 var meow = require('meow');
 var finderTag = require('./');
 
-var cli = meow([
-  'Usage',
-  '  $ finder-tag [path] [color]',
-  '',
-  'Available Colors:',
-  '  `gray`, `green`, `purple`, `blue`, `yellow`, `red`, `orange`',
-  '  To clear all tags pass \`clear\` as the color',
-  '',
-  'Examples',
-  '  $ finder-tag ~/Desktop blue',
-  '  /Users/dave/Downloads is tagged `blue`',
-  '  $ finder-tag app.js yellow',
-  '  /root/site/scripts/app.js is tagged `yellow`',
-  '  $ finder-tag app.js clear',
-  '  /root/site/scripts/app.js all tags have been cleared'
-]);
-
-finderTag(cli.input[0], cli.input[1]).then(
-  (obj) =>
-    obj.tag === 'clear' ?
-      console.log(`${obj.path} all tags have been cleared`) :
-      console.log(`${obj.path} is tagged \`${obj.tag}\``)
+var cli = meow(
+  [
+    'Usage',
+    '  $ finder-tag [path] [color]',
+    '',
+    'Available Colors:',
+    '  `gray`, `green`, `purple`, `blue`, `yellow`, `red`, `orange`',
+    '  To clear all tags pass `clear` as the color',
+    '',
+    'Examples',
+    '  $ finder-tag ~/Desktop blue',
+    '  /Users/dave/Downloads is tagged `blue`',
+    '  $ finder-tag app.js yellow',
+    '  /root/site/scripts/app.js is tagged `yellow`',
+    '  $ finder-tag app.js clear',
+    '  /root/site/scripts/app.js all tags have been cleared'
+  ].join('\n')
 );
+
+if (cli.input[0] && cli.input[0]) {
+  finderTag(cli.input[0], cli.input[1]).then(
+    obj =>
+      obj.tag === 'clear'
+        ? console.log(`${obj.path} all tags have been cleared`)
+        : console.log(`${obj.path} is tagged \`${obj.tag}\``)
+  );
+} else {
+  console.log('Invalid input');
+  console.log(cli.help);
+}
--- test.js
+++ test.js
@@ -11,59 +11,55 @@ let tildePath;
 test('absolute path', t => {
   const pkg = 'ava';
   absolutePath = path.join(nm, pkg);
-  return fn(absolutePath, 'blue')
-    .then(res => {
-      t.is(res.code, 0);
-      t.is(res.tag, 'blue');
-      t.is(res.path, absolutePath);
-      t.ok(res.command.length > 10);
-    }, t.fail);
+  return fn(absolutePath, 'blue').then(res => {
+    t.is(res.code, 0);
+    t.is(res.tag, 'blue');
+    t.is(res.path, absolutePath);
+    t.true(res.command.length > 10);
+  }, t.fail);
 });
 
 test('relative path', t => {
   const pkg = 'pify';
   relativePath = './node_modules/' + pkg;
-  return fn(relativePath, 'green')
-    .then(res => {
-      t.is(res.code, 0);
-      t.is(res.tag, 'green');
-      t.is(res.path, path.join(nm, pkg));
-      t.ok(res.command.length > 10);
-    }, t.fail);
+  return fn(relativePath, 'green').then(res => {
+    t.is(res.code, 0);
+    t.is(res.tag, 'green');
+    t.is(res.path, path.join(nm, pkg));
+    t.true(res.command.length > 10);
+  }, t.fail);
 });
 
 test('tilde path', t => {
   tildePath = '~/Desktop';
-  return fn(tildePath, 'yellow')
-    .then(res => {
-      t.is(res.code, 0);
-      t.is(res.tag, 'yellow');
-      t.ok(res.path.includes('/Users/'));
-      t.ok(res.command.length > 10);
-    }, t.fail);
+  return fn(tildePath, 'yellow').then(res => {
+    t.is(res.code, 0);
+    t.is(res.tag, 'yellow');
+    t.true(res.path.includes('/Users/'));
+    t.true(res.command.length > 10);
+  }, t.fail);
 });
 
 test('path does not exist', t =>
-  fn(path.join(__dirname, 'node_modules/doesnotexist'), 'blue')
-    .then(t.fail, err => t.is(err.code, 'ENOENT'))
-);
+  fn(path.join(__dirname, 'node_modules/doesnotexist'), 'blue').then(
+    t.fail,
+    err => t.is(err.code, 'ENOENT')
+  ));
 
 test('color does not exist', t =>
-  fn(absolutePath, 'hotpink')
-    .then(t.fail, err => t.ok(err.message.includes('color must be one of')))
-);
+  fn(absolutePath, 'hotpink').then(t.fail, err =>
+    t.true(err.message.includes('color must be one of'))
+  ));
 
 test('path is not string', t =>
-  fn(92403, 'hotpink')
-    .then(t.fail, err => t.ok(err.message.includes('path should be a string')))
-);
+  fn(92403, 'hotpink').then(t.fail, err =>
+    t.true(err.message.includes('path should be a string'))
+  ));
 
 test.after('clear test tags', () =>
-  Promise.all(
-    [
-      fn(absolutePath, 'clear'),
-      fn(relativePath, 'clear'),
-      fn(tildePath, 'clear')
-    ]
-  )
+  Promise.all([
+    fn(absolutePath, 'clear'),
+    fn(relativePath, 'clear'),
+    fn(tildePath, 'clear')
+  ])
 );
